e68a0a3a68cac281559fc0043a918dc118c3ef8a
Zack Pace <uiysgamer@gmail.com>
2020-05-24 23:29:08 -0600

    Init commit.

---
+++ src/Route.js
@@ -0,0 +1,8 @@
+module.exports = class {
+    constructor(path, func) {
+        this.path = path;
+        this.func = func;
+    }
+
+    
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+let http = require('http');
+
+let bolt = function() {
+    
+}
+
+let server = http.createServer(function(request, response) {
+
+});
